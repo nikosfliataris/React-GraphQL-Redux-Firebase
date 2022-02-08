8fea603784ec1995943c66d24c60913e9720ae52
nikosfliataris <75726267+nikosfliataris@users.noreply.github.com>
2022-02-08 11:03:23 +0200

    Update Payment.js

--- src/Page/Payment/Payment.js
+++ src/Page/Payment/Payment.js
@@ -4,9 +4,7 @@ import "./Payment.scss";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import PaymentDetails from "../../Component/PaymentDetails/PaymentDetails";
-const stripePromise = loadStripe(
-  "pk_test_51KOeqdKNJWPh15dDwiQAAiEYgsFdBYUomPoGF0CGCO6TkoCA54xjdpm90kiziTCw0gvH0e0eVq0eyY49tkZCmpcm00KtZAsXw7"
-);
+const stripePromise = loadStripe("Stripe_Public_key");
 function Payment() {
   return (
     <Elements stripe={stripePromise}>
